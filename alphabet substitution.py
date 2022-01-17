f7e41d63a013d8faae294aaeb207f99db3edb5f7
tranqinspirit <6962396+tranqinspirit@users.noreply.github.com>
2022-01-16 23:33:57 -0600

    Add files via upload

---
+++ alphabet substitution.py
@@ -0,0 +1,49 @@
+# simple substitution cipher of sorts
+import random
+step = random.randrange(1,3,1)
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+max_length = len(alphabet) - 1
+
+def cipher(word, step):
+    cipher = word
+    counter = 0
+    for x in cipher:
+        if x != ' ':
+            index = alphabet.index(cipher[counter])
+            
+            check = -1
+       
+            if (index + step >= max_length):
+                check = ((index + step) - index)
+                check = check - 1
+            else:
+                check = index + step
+           
+            cipher[counter] = alphabet[check]
+            
+        counter = counter + 1
+    
+    return cipher
+
+def convertlisttostring(s):
+    new = ""
+
+    for x in s:
+        new  += x
+    return new
+
+def main():
+    loop = True
+    while loop:
+        word = input("Enter word: ")
+
+        word = word.lower()
+        word = list(word)
+        new_word = cipher(word, step)
+        new_word = convertlisttostring(new_word)
+        print(str(step) + ' ' + new_word)
+
+if __name__ == "__main__":
+    main()
