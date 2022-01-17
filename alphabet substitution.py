a2a9b11ecf5ad9690054c3669001c710e20cd39b
tranqinspirit <6962396+tranqinspirit@users.noreply.github.com>
2022-01-17 15:48:08 -0600

    Update alphabet substitution.py
    fix the wrapping around after z, oops

--- alphabet substitution.py
+++ alphabet substitution.py
@@ -3,7 +3,7 @@ import random
 
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-max_length = len(alphabet) - 1
+max_length = len(alphabet) #- 1
 
 def cipher(word, step):
     cipher = word
