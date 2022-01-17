d968702ba21e3fb2ba1602099ee165f2f3092933
tranqinspirit <6962396+tranqinspirit@users.noreply.github.com>
2022-01-17 11:52:48 -0600

    Update alphabet substitution.py
    step wasn't randomizing properly but this fixes that

--- alphabet substitution.py
+++ alphabet substitution.py
@@ -1,6 +1,5 @@
 # simple substitution cipher of sorts
 import random
-step = random.randrange(1,3,1)
 
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
@@ -37,7 +36,8 @@ def convertlisttostring(s):
 def main():
     loop = True
     while loop:
-        word = input("Enter word: ")
+        word = input("Enter message: ")
+        step = random.randrange(1,3,1)
 
         word = word.lower()
         word = list(word)
